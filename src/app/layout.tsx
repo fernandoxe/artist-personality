dfb0b3fef7541f5b29858d8e1489992356b2d1ab
Fernando Parra <fernandoxe@gmail.com>
2023-05-19 16:59:39 +0200

    chore: lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="es">
+      <body className={`${inter.className} bg-[#ac9eb8]`}>{children}</body>
     </html>
   )
 }
